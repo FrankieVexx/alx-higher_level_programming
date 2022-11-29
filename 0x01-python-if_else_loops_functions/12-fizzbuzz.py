a0ea12d8ea7fb1b3c30fb2bc5e3f07a2ef4b95cd
Francis Onyach <franblog7@gmail.com>
2022-11-29 09:04:13 -0800

    changes

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3                                                                                       
-
 #Task 12 - FizzBuzz.py                                                                                   
-
 #Francis O. Onyach <franblog7@gmail.com>
+
 def fizzbuzz():
     """Print numbers from 1 to 100 separated with a space
     For multiples of 3 print Fizz in place of the number
     For multiples of 5 print Buzz in place of the number
-    For multiplesof 3 and 5 print FIzzBuzz in place of the number """
+    For multiplesof 3 and 5 print FIzzBuzz in place of the number 
+    """
 
 for number in range(1, 101):
         if number % 3 == 0 and number % 5 == 0:
