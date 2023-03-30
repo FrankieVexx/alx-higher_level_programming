0f3f3096f625db29689c4eedab20c0fe53e8eeff
Francis Onyach <franblog7@gmail.com>
2023-03-30 11:21:27 -0700

    corrections to task 1

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# send a request to an URL with curl and displays the body of the response
-curl -s -o /dev/null -w "%{http_code}" "$1" | grep -q 200 && curl -s "$1"
+# send a GET request to an URL with curl, and display the body of the response
+curl -sL "$1"
