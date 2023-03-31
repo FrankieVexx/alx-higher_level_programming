c45e72ef3c333f190e036d3d2f1602bd359ea224
Francis Onyach <franblog7@gmail.com>
2023-03-31 05:22:51 -0700

    changes to task 5

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -1,9 +1,13 @@
 #!/usr/bin/python3
-"""retrieve a header from a get request"""
-
-import requests
-from sys import argv
+"""takes in a URL, sends a request to the URL and displays
+the value of the X-Request-Id variable found in the
+header of the response
+"""
 
 if __name__ == "__main__":
-    response = requests.get(argv[1])
-    print(response.headers['X-Request-Id'])
+    import requests
+    from sys import argv
+
+    url = argv[1]
+    res = requests.get(url)
+    print(res.headers.get('X-Request-Id'))
