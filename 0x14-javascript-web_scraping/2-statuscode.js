f1c057c78a327dbe0986e7016772ac0906d8ce02
Francis Onyach <franblog7@gmail.com>
2023-04-25 05:07:04 -0700

    populate to_do_all list

---
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+// a script to print status code
+
+const request = require('request');
+
+const url = process.argv[2];
+
+request(url, (err, res, body) => {
+  if (err) {
+    console.error(err);
+  }
+  console.log('code:', res.statusCode)
+});
