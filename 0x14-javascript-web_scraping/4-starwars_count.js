17f4c388ab6569218b76ec6548f1116606cee945
Francis Onyach <franblog7@gmail.com>
2023-04-25 07:24:32 -0700

    print number of movies

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -6,10 +6,10 @@ const request = require('request');
 const url = process.argv[2];
 request(url, (err, res, body) => {
   if (err) console.error(err);
-  const data = JSON.parse(body);
+  const content = JSON.parse(body);
   let count = 0;
-  for (let i = 0; i < data.results.length; i++) {
-    const chars = data.results[i].characters;
+  for (let i = 0; i < content.results.length; i++) {
+    const chars = content.results[i].characters;
     for (let k = 0; k < chars.length; k++) {
       if (chars[k].includes('18')) {
         count = count + 1;
